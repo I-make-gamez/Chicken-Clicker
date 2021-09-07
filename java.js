7f83121df8d81fa7dc23596e8399c7e5ea7b2f41
I-make-gamez <ebelcher143@gmail.com>
2021-09-07 17:02:45 +0000

    hi

--- java.js
+++ java.js
@@ -2,4 +2,9 @@ var eggs = 0;
 var eps = 0;
 
 const click = document.getElementById("click");
-const eggs = document.getElementById("clicks");
+const egg = document.getElementById("clicks");
+
+click.addEventListener("click", function(){
+    eggs += 1;
+    egg.innerHTML = "Eggs: " + eggs
+});
