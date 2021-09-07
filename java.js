b99063fffa2183f9ccd24334dc08059836a177ff
I-make-gamez <ebelcher143@gmail.com>
2021-09-07 17:06:46 +0000

    CC-002

--- java.js
+++ java.js
@@ -1,10 +1,11 @@
 var eggs = 0;
 var eps = 0;
+var filler = null;
 
 const click = document.getElementById("click");
 const egg = document.getElementById("clicks");
 
 click.addEventListener("click", function(){
     eggs += 1;
-    egg.innerHTML = "Eggs: " + eggs
+    egg.innerHTML = "Eggs: " + eggs;
 });
